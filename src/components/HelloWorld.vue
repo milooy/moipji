6ee4c4e0146fc88ebfb2defdfc2a0b50d8cf7fe8
Jay Jin <jayjinjay@gmail.com>
2019-11-02 18:50:35 +0900

    Show clothes based on the temparature

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,96 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <h1>{{ cityName }}에서 오늘 모입지?</h1>
+    <div>Lat: {{ lat }}</div>
+    <div>Lon: {{ lon }}</div>
+    <div>cityName: {{ cityName }}</div>
+    <div>temperature: {{ temperature }}</div>
+    <div>clothes: {{ clothes }}</div>
   </div>
 </template>
 
@@ -98,7 +13,68 @@
 export default {
   name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
+    lat: Number,
+    lon: Number,
+    cityName: String,
+    temperature: String,
+  },
+  created: function () {
+    console.log("created");
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.lat = position.coords.latitude
+      this.lon = position.coords.longitude
+      this.fetchWeather(this.lat, this.lon);
+    });
+  },
+  computed: {
+    clothes: function () {
+      const temp = this.temperature;
+      console.log("컴퓨티드", temp, temp < 4)
+
+      if (temp >= 28) {
+        return "민소매, 반팔, 반바지, 짧은 치마, 린넨 옷";
+      }
+      if (temp >= 23) {
+        return "반팔, 얇은 셔츠, 반바지, 면바지";
+      }
+      if (temp >= 20) {
+        return "블라우스, 긴팔 티, 면바지, 슬랙스, ";
+      }
+      if (temp >= 17) {
+        return "얇은 셔츠나 가디건/니트, 맨투맨, 후드, 긴 바지";
+      }
+      if (temp >= 12) {
+        return "자켓, 가디건, 청자켓, 니트, 스타킹, 청바지";
+      }
+      if (temp >= 9) {
+        return "트렌치 코트, 야상, 점퍼, 스타킹, 기모바지";
+      }
+      if (temp >= 5) {
+        return "울 코트, 히트텍, 가죽 옷, 기모";
+      }
+      if (temp < 5) {
+        return "패딩, 두꺼운 코트, 누빔 옷, 기모, 목도리";
+      }
+      return "로딩중";
+    }
+  },
+  methods: {
+    // 계산된 getter
+    fetchWeather: function (lat, lon) {
+      const that = this;
+      fetch(`http://api.openweathermap.org/data/2.5/weather?appid=0f4f911a6ae37361da7bfcf43622bb61&lang=KR&units=metric&lat=${lat}&lon=${lon}`)
+        .then(res => {
+          console.log({res})
+          return res.json();
+        })
+        .then(data => {
+          console.log({data}, that)
+          that.cityName = data.name;
+          that.temperature = data.main.temp;
+        });
+    }
   }
 };
 </script>
